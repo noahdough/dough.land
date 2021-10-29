d710c684c6c463a059fca65995471f5b757d5ba9
Noah <noah@dough.land>
2021-10-29 23:58:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// GSAP
+// gsap.from(".container", { duration: 1, y: 200 });
+gsap.from(".container", { duration: 1, y: 300 });
+gsap.from(".nav-item", { duration: 1, x: 300, delay: 1 });
+
+let i = 0;
+let txt = "Welcome to Dough.Land";
+let speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".welcome").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
